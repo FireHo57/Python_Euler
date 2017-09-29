873346406cc64b007694dafb9f1128407fa2624a
Me <charlie_haddock@hotmail.co.uk>
2017-09-29 22:55:49 +0100

    And solved!

--- problem_12/problem_12_main.py
+++ problem_12/problem_12_main.py
@@ -10,9 +10,9 @@ def main():
 
     # would like to get an idea of how divisors increase with triangle number
     t1=t.time()
-    primes=pf.sieve_of_e(100000)
-    triangles=tf.get_n_triangles(100000)
-    print(tf.get_nth_triangle(100000))
+    primes=pf.sieve_of_e(50000)
+    triangles=tf.get_n_triangles(50000)
+    print(tf.get_nth_triangle(50000))
     x_axis=[]
     y_axis=[]
     x_axis.append(1)
@@ -23,6 +23,9 @@ def main():
 
     t2=t.time()
     print(t2-t1)
+
+    over_500_index=y_axis.index( next(i for i in y_axis if i > 500 ) )
+    print("First triangle number with over 500 divisors: {}".format(x_axis[over_500_index]))
     plt.plot(x_axis,y_axis)
     plt.show()
 
