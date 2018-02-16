971ce4fbb221e86042bbb420baa3dde53c055604
Me <charlie_haddock@hotmail.co.uk>
2018-02-16 07:46:17 +0000

    Found a few small bugs fixed them and solved 17

--- problem_17/number_letter_counts.py
+++ problem_17/number_letter_counts.py
@@ -12,22 +12,34 @@ if __name__=="__main__":
     for i in numbers_to_nine:
         letters_to_nine+=len(i)
 
+    print "Numbers to nine: {}".format(letters_to_nine)
+
     letters_in_teens=0
     for i in ten_to_twenty:
         letters_in_teens+=len(i)
 
-    letters_in_hundred=0
-    for i in tens:
-        letters_in_hundred+=(10*len(i))+(letters_to_nine*9)
+    print "Letters in teens: {}".format(letters_in_teens)
 
+    letters_in_hundred=0
+    # for 20-90: twentyone,twentytwo...twentynine
+    for i in tens:
+        letters_in_hundred+=(10*len(i))+(letters_to_nine)
+
+    #add in numbers 1-19
     letters_in_hundred+=letters_to_nine+letters_in_teens
+
+    print "Letters in hundred: {}".format(letters_in_hundred)
 
     letters_in_thousand=0
     hundred=len("hundred")
+    # for 100-999
     for i in numbers_to_nine:
-        letters_in_thousand+=(len(i)+3)*100+hundred+letters_in_hundred
+        # i hundred, i hundred and ...
+        letters_in_thousand+= len(i)+hundred+(len(i)+hundred+3)*99+letters_in_hundred
 
+    #1-99
+    letters_in_thousand+=letters_in_hundred
     letters_in_thousand+=len("onethousand")
 
-    print letters_in_thousand
+    print "Letters in thousand: {}".format(letters_in_thousand)
 
