ad9c39e2373649378986cb7c9d7342c024485b6d
Me <charlie_haddock@hotmail.co.uk>
2018-02-23 08:27:48 +0000

    Finished problem 18, this will apparently also work on problem 67...

--- problem_18/main.py
+++ problem_18/main.py
@@ -13,22 +13,32 @@ def str_to_int_arrays(input_array):
 
     return output_array
 
+# assumes that arrays are either square or if triangular then longer rows are at a lower index
+def get_highest_adjacent(row,column,array):
+    #the two adjacents will be the row below and same column and row below and column +1
+    adj1 = array[row-1][column]
+    adj2 = array[row-1][column+1]
+    return max(adj1,adj2)
+
 def create_weighted_array(int_array):
 
-    weighted_array=int_array
-    for i in reversed(weighted_array[0:-1]):
-        # for each row add the previously weighted adjacents
-        for j,i in enumerate(i):
-            print "j: {} i: {}".format(j,i)
+    weighted_array=int_array[:]
+    weighted_array.reverse()
 
+    for i in range (1,len(weighted_array)):
+        for j in range (len(weighted_array[i])):
+            weighted_array[i][j]+=get_highest_adjacent(i,j,weighted_array)
+
+    return weighted_array
 
 
 def main():
     array = fr.read_to_array("test_data.txt")
 
     int_array = str_to_int_arrays(array)
-    create_weighted_array(int_array)
-
+    new_array = create_weighted_array(int_array)
+    for i in new_array:
+        print i
 
 
 if __name__ == "__main__":
